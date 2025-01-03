df228c92ec751105e3c4d55faad5a982905f3e90
Leonardo Rudolf Manangka <leonardo.manangka@delameta.com>
2025-01-03 10:39:40 +0700

    add personal config for nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- main requirement
+-- example: require("user.lazy")
+require("leo.lazy")
+
+-- colorscheme
+vim.cmd("colorscheme gruvbox") -- available "kanagawa", "gruvbox" and "tokyonight"
+vim.o.background = "dark" -- set to "dark" or "light" mode
